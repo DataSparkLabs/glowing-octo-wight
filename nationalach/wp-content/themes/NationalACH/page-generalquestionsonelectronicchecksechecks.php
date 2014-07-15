a99a5f48320496ec36a5eabc2c6af8f573272957
cto7182 <Clayton@Claytons-MacBook-Pro.local>
2014-07-15 18:54:22 -0500

    Updated table infos and begin updating FAQ

---
+++ nationalach/wp-content/themes/NationalACH/page-generalquestionsonelectronicchecksechecks.php
@@ -0,0 +1,35 @@
+<?php
+/*
+  Template Name: General Questions eCheck
+ */
+?>
+<?php get_header(); ?>
+<div id="content">
+    <div class="inner">
+        <div class="sidebar">
+             <ul>
+                <?php dynamic_sidebar('Page Sidebar') ?>
+            </ul>
+        </div>
+        <div class="content-top"></div>
+        <div class="content-left">
+
+        <? $query = new WP_Query( array( 'post_type' => 'faq', 'faq_tax' => 'general-questions-on-electronic-checks-echecks' ) ); ?>
+            
+               <?php while($query->have_posts() ) : $query->the_post(); ?>
+          <div class="post"> 
+               <h1><?php the_title(); ?></h1>
+               <div class="entry">    
+                    <?php the_content(); ?>
+                    
+                    <?php  wp_reset_postdata(); ?>
+                    
+              </div>
+          </div>
+     <?php endwhile; ?>
+
+        </div>
+        <div class="content-bottom"></div>
+    </div>
+</div>
+<?php get_footer(); ?> 
